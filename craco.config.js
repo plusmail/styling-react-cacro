1fe7e3031c400f0476e138c3333c449467db5bd9
plusmail <plus4957@gmail.com>
2024-11-22 09:18:50 +0900

    111

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+    webpack: {
+        alias: {
+            "@styles": path.resolve(__dirname, "src/styles"), // Less 스타일 경로 별칭 설정
+        },
+    },
+    // style: {
+    //     less: {
+    //         loaderOptions: {
+    //             lessOptions: {
+    //                 javascriptEnabled: true, // JavaScript 내장 기능 활성화 (필요시)
+    //             },
+    //         },
+    //     },
+    // },
+};
---
+++ src/SassComponent.js
@@ -0,0 +1,19 @@
+
+import "@styles/SassComponent.scss";
+// import "@styles/SassComponentCSS.css";
+
+
+export const SassComponent = ()=>{
+
+    return (
+        <div className={"SassComponent"}>
+            <div className={"box red"}>dddddddddddddddd</div>
+            <div className={"box orange"}/>
+            <div className={"box yellow"}/>
+            <div className={"box green"}/>
+            <div className={"box blue"}/>
+            <div className={"box indigo"}/>
+            <div className={"box violet"}/>
+        </div>
+    )
+}
